cfdda07b69d2a99ab5dceaa5cee74347e752feac
Matheus Khairallah <61334878+MatheusKhairallah@users.noreply.github.com>
2020-07-03 21:17:31 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace SequenciaCentena
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+            Console.WriteLine("Digite qualquer tecla para mostrar os 100 primeiros números naturais.");
+            Console.WriteLine("*Lembrete*: digite S para sair e N para continuar");
+            Console.WriteLine("Digite qualquer tecla para continiar");
+            Console.ReadKey();
+            Console.Clear();
+            
+            
+            string sair;
+            do
+            {
+                Console.Write("Deseja sair S ou N?");
+            sair=Console.ReadLine().ToUpper();
+                Console.WriteLine("1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30 31 32 33 34 35 36 37 38 39 40 41 42 43 44 45 46 47 48 49 50 51 52 53 54 55 56 57 58 59 60 61 62 63 64 65 66 67 68 69 70 71 72 73 74 75 76 77 78 79 80 81 82 83 84 85 86 87 88 89 90 91 92 93 94 95 96 97 98 99 100");
+
+            } while (sair != "S" );
+
+            Console.Clear();
+            Console.WriteLine("Fim! Obrigado por testar o programa!");
+            
+                
+            
+            
+            
+            
+                
+            
+            
+
+        }
+    }
+}
